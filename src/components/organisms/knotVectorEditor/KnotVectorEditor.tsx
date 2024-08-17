ccdba64b2d06d3f6a0f1342c1cf1d3158cd40672
Eric Demers <eric.demers@polymtl.ca>
2024-08-17 14:39:45 -0400

    showing basis functions

--- src/components/organisms/knotVectorEditor/KnotVectorEditor.tsx
+++ src/components/organisms/knotVectorEditor/KnotVectorEditor.tsx
@@ -3,9 +3,22 @@
 //https://stackoverflow.com/questions/66254850/how-to-resize-canvas-according-to-the-parent-div-element-react
 
 import type { FunctionComponent } from "react"
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react"
 import { useAppSelector } from "../../../app/hooks"
-import { selectTheme } from "../../templates/sketcher/sketcherSlice"
+import {
+  selectControlPolygonsDispayed,
+  selectTheme,
+} from "../../templates/sketcher/sketcherSlice"
+import { CurveType, type Curve } from "../../../sketchElements/curveTypes"
+import { computeBasisFunction } from "./basisFunctions"
+import { createColorPaletteRGB } from "../../../utilities/color"
+import { selectCurves } from "../../../sketchElements/sketchElementsSlice"
 
 /*
 interface EditorProps {
@@ -16,13 +29,48 @@ interface EditorProps {
 }
 */
 
+type KnotEditorStateType =
+  | "idle"
+  | "moving a knot"
+  | "selected knot"
+  | "display position on abscissa"
+type ActionType = "none" | "zooming" | "scrolling"
+type mouseMoveThresholdType = "not exceeded" | "just exceeded" | "exceeded"
+
 //const KnotVectorEditor: FunctionComponent<EditorProps> = props => {
 const KnotVectorEditor = () => {
   //const { editorWidth, editorHeight, offsetX, offsetY } = props
+  const [curve, setCurve] = useState<Curve | null>(null)
+  const curves = useAppSelector(selectCurves)
+  const controlPolygonsDisplayed = useAppSelector(selectControlPolygonsDispayed)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const [width, setWidth] = useState(0)
   const [height, setHeight] = useState(0)
   const theme = useAppSelector(selectTheme)
+  const [zoom, setZoom] = useState(1)
+  const [action, setAction] = useState<ActionType>("none")
+  const [scroll, setScroll] = useState(0)
+  const [pixelRatio, setPixelRatio] = useState<number>(1)
+  const maximumZoom = 10
+  const leftMaximumSliderPosition = 0.35
+  const rightMaximumSliderPosition = 0.65
+  const reductionFactor = 0.9
+  const clickWithoutMovingResolution = 0.0005
+
+  useLayoutEffect(() => {
+    setPixelRatio(Math.ceil(window.devicePixelRatio))
+  }, [])
+
+  useLayoutEffect(() => {
+    if (controlPolygonsDisplayed === null) {
+      setCurve(null)
+      return
+    }
+    const selectedCurves = curves.filter(curve =>
+      controlPolygonsDisplayed.curveIDs.includes(curve.id),
+    )
+    setCurve(selectedCurves[0])
+  }, [controlPolygonsDisplayed, curves])
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -35,6 +83,44 @@ const KnotVectorEditor = () => {
     return () => resizeOberver.disconnect()
   }, [])
 
+  const drawBasisFunctions = useCallback(
+    (context: CanvasRenderingContext2D, curve: Curve, ratio: number = 1) => {
+      const scaleX = reductionFactor * zoom
+      const scaleY = 0.5
+      const offsetLeft = 0.05 + reductionFactor * scroll
+      const offsetTop = 0.7 * ratio
+      const colorPalette = createColorPaletteRGB(curve.points.length, 1)
+      switch (curve.type) {
+        case CurveType.NonRational:
+          {
+            const basisFunctions = computeBasisFunction(curve)
+            context.strokeStyle = colorPalette[0]
+            context.lineJoin = "round"
+            context.lineWidth = 1.6 / width
+            basisFunctions.forEach((b, index) => {
+              if (b[0] !== undefined) {
+                context.beginPath()
+                context.strokeStyle = colorPalette[index]
+                context.moveTo(
+                  b[0].u * scaleX + offsetLeft,
+                  -b[0].value * ratio * scaleY + offsetTop,
+                )
+                b.forEach((point, index) =>
+                  context.lineTo(
+                    point.u * scaleX + offsetLeft,
+                    -point.value * ratio * scaleY + offsetTop,
+                  ),
+                )
+                context.stroke()
+              }
+            })
+          }
+          break
+      }
+    },
+    [scroll, width, zoom],
+  )
+
   useLayoutEffect(() => {
     const canvas = canvasRef.current
     if (!canvas) return
@@ -42,8 +128,12 @@ const KnotVectorEditor = () => {
     const context = canvas.getContext("2d")
     if (!context) return
     context.save()
-    context.clearRect(0, 0, height, width)
-    context.scale(width, width)
+    context.clearRect(0, 0, width * pixelRatio, height * pixelRatio)
+    context.scale(width * pixelRatio, width * pixelRatio)
+
+    if (curve) {
+      drawBasisFunctions(context, curve, ratio)
+    }
 
     const lineColor =
       theme === "dark" ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 1)"
@@ -63,16 +153,15 @@ const KnotVectorEditor = () => {
     context.lineTo(0.97, 0.85 * ratio)
     context.stroke()
     context.restore()
-    console.log("test")
-  }, [canvasRef, height, theme, width])
+  }, [canvasRef, curve, drawBasisFunctions, height, pixelRatio, theme, width])
 
   return (
     <div className="shadow rounded-lg bg-white dark:bg-neutral-800 h-full w-full">
       <canvas
         className="w-full h-full pointer-events-auto"
         ref={canvasRef}
-        height={height}
-        width={width}
+        height={height * pixelRatio}
+        width={width * pixelRatio}
       />
     </div>
   )
---
+++ src/components/organisms/knotVectorEditor/basisFunctions.ts
@@ -0,0 +1,35 @@
+import { basisFunctions } from "../../../bSplineAlgorithms/Piegl_Tiller_NURBS_Book";
+import type { Curve } from "../../../sketchElements/curveTypes";
+
+export function computeBasisFunction(curve: Curve, step: number = 0.001) {
+    const numberOfControlPoints = curve.points.length
+    let result: {u: number, value: number}[][] = []
+    for (let i = 0; i < numberOfControlPoints; i += 1) {
+        result.push([])
+    }
+    const degree = curve.degree === undefined ? curve.knots.length - curve.points.length - 1 : curve.degree
+    if (degree > 0) {
+        curve.knots.forEach((value, knotIndex) => {
+            if (knotIndex < curve.knots.length - 1 && value !== curve.knots[knotIndex + 1]) {
+                const range = rangeIncludingStopValue(value, curve.knots[knotIndex + 1], step)
+                range.forEach(u => {
+                    const basis = basisFunctions(knotIndex, u, curve.knots, degree)
+                    basis.forEach((value, index) => {
+                        result[knotIndex + index - degree].push({u, value})
+                    })
+                })
+            }
+        })
+    }
+    return result
+}
+
+
+export function rangeIncludingStopValue(start: number, stop: number, step: number) {
+    let result: number[] = []
+    for (let i = start; i <= stop; i += step) {
+        result.push(i)
+    }
+    if (result[result.length - 1] !== stop) result.push(stop)
+    return result
+}
